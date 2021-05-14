59cc5d5113013231967f4416b28ba28b0f3c6713
IMPrimph <vishnugajulapalli123@gmail.com>
2021-05-14 15:20:54 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes')
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// json
+// takes in json data that comes along with the request and parses it into javascript object
+// and attaches that data to the request object inside the handler for that route
+app.use(express.json())
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://primph:noobie@cluster0.2mjwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then((result) => {
+    app.listen(3000, () => {
+      console.log('Server started running')
+      console.log('DB Connected')
+    })
+  })
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+app.use(authRoutes)
