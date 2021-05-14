85fcfb1017b63594e6061543a9501dc4c3b6b8c9
IMPrimph <vishnugajulapalli123@gmail.com>
2021-05-14 20:45:32 +0530

    logging in users from frontend

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,12 +3,23 @@ const jwt = require('jsonwebtoken')
 
 // handle errors
 const handleErrors = (err) => {
+    console.log(err.message, err.code)
     let errors = { email: "", password: "" }
 
     // duplicate error code
     if (err.code === 11000) {
         errors.email = 'That email is already registered'
         return errors
+    }
+
+    // incorrect email
+    if (err.message === 'Incorrect email') {
+        errors.email = 'That email is not registered'
+    }
+
+    // incorrect password
+    if (err.message === 'Incorrect Password') {
+        errors.email = 'That password is incorrect'
     }
 
     // validation errors
@@ -55,6 +66,19 @@ module.exports.signup_post = async (req, res) => {
 
 module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
-    console.log(email, password)
-    res.send('User login')
+
+    try {
+        const user = await User.login(email, password)
+        const token = createToken(user._id)
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            maxAge: maxAge * 1000
+        })
+        res.status(200).json({ user: user._id })
+    } catch (err) {
+        const errors = handleErrors(err)
+        res.status(400).json({
+            errors
+        })
+    }
 }
